739613091ee8412edbf6123a4481cfb37ae2a6d7
bschorchit <b.schorchit@gmail.com>
2021-11-01 10:53:22 -0300

    atualizei art e dei clean no app

---
+++ contracts/Art.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.7;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721Full.sol';
+
+contract Art is ERC721Full {
+
+  bytes3[] public arts;
+  mapping(bytes3 => bool) private _artExists;
+
+  constructor() ERC721Full("Art", "ART") public {
+  }
+
+  // E.G. art = "#FFFFFF"
+  function mint(bytes3 _art) public {
+    require(!_artExists[_art], "art exists");
+    uint _id = arts.push(_art);
+    _mint(msg.sender, _id);
+    _artExists[_art] = true;
+  }
+}
